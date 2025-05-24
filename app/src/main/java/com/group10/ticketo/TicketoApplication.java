94b49135b87c33acdfa44b592d327ff27653f835
Rodrigo <rfnobando@gmail.com>
2025-05-23 21:38:29 -0300

    First commit

---
+++ app/src/main/java/com/group10/ticketo/TicketoApplication.java
@@ -0,0 +1,13 @@
+package com.group10.ticketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketoApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/group10/ticketo/TicketoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group10.ticketo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
