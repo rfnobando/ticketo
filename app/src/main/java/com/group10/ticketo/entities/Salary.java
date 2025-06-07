7cb2b2d4d15e53ced149e93456ffa1c1b6c6f963
AlanLeonelMaciel <alanleonelmaciel@gmail.com>
2025-06-06 21:19:21 -0300

    Se repara lo que se trajo de la rama trunk para empezar a trabajar en security

--- app/src/main/java/com/group10/ticketo/entities/Salary.java
+++ app/src/main/java/com/group10/ticketo/entities/Salary.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pay  {
+public class Salary {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
