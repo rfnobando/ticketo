7ea2c3d7d8ea8c964b9fdd33396e7c51cb8326ff
EmilianoGinarte <eginartedelgado@gmail.com>
2025-06-09 22:32:59 -0300

    Reutilizacion de vista

--- app/src/main/java/com/group10/ticketo/services/ITicketService.java
+++ app/src/main/java/com/group10/ticketo/services/ITicketService.java
@@ -2,13 +2,6 @@ package com.group10.ticketo.services;
 
 import com.group10.ticketo.dtos.CreateTicketDTO;
 import com.group10.ticketo.dtos.TicketDTO;
-import com.group10.ticketo.dtos.TicketWithCategoryDTO;
-import com.group10.ticketo.entities.Customer;
-import com.group10.ticketo.entities.Employee;
-import com.group10.ticketo.entities.TicketCategory;
-
-import com.group10.ticketo.entities.Ticket;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,7 +9,7 @@ public interface ITicketService {
     //Traer Lista de Tickets creados por un cliente
     List<TicketDTO> findByCustomerId(Long customerId);
 
-    public List<TicketWithCategoryDTO> findTicketsByDepartmentId(Long departmentId);
+    public List<TicketDTO> findTicketsByDepartmentId(Long departmentId);
 
     //Crear Ticket que viene del controller
     void createTicket(CreateTicketDTO createTicketDTO) throws Exception;
